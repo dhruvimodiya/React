36863e86926270c33a91d18722464bac59df626c
Dhruvi Modiya <dmodiya512@gmail.com>
2024-05-06 16:07:30 +0530

    react

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+      <div className='w-screen h-screen'
+       style={{backgroundColor:color}}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button className='outline-none px-4' style={{backgroundColor:"red"}}>Red</button>
+          </div>
+        </div>
+       </div>
+  )
+}
+
+export default App
