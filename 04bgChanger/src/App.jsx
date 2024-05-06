a310d9d905197d240c8a326e5bc2ff0e3bb6adf0
Dhruvi Modiya <dmodiya512@gmail.com>
2024-05-06 16:55:24 +0530

    react

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -11,7 +11,12 @@ function App() {
        style={{backgroundColor:color}}>
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-            <button className='outline-none px-4' style={{backgroundColor:"red"}}>Red</button>
+            <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white' onClick={()=> setColor("red")} style={{backgroundColor:"red"}}>Red</button>
+            <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white' onClick={()=> setColor("green")} style={{backgroundColor:"green"}}>Green</button>
+            <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white' onClick={()=> setColor("blue")} style={{backgroundColor:"blue"}}>Blue</button>
+            <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white' onClick={()=> setColor("pink")} style={{backgroundColor:"pink"}}>Pink</button>
+            <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white' onClick={()=> setColor("orange")}style={{backgroundColor:"orange"}}>Orange</button>
+            <button className='outline-none px-4 py-1 rounded-full shadow-lg text-white' onClick={()=> setColor("yellow")} style={{backgroundColor:"yellow"}}>Yellow</button>
           </div>
         </div>
        </div>
