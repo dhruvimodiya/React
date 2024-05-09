6272d1174a54be3bf0bac72a2c3ade911c0100c0
Dhruvi Modiya <dmodiya512@gmail.com>
2024-05-09 16:12:57 +0530

    react router

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
+  return (
+    <div className='text-center m-4 bg-orange-500 text-white p-4 text-2xl'>
+    <p className='text-red-950 font-weight:700 text-3xl'>This information is about hitesh choudhary</p>
+    <p>type : {data.type}</p>
+    <p>Github followers: {data.followers}</p>
+    <p >{data.bio}</p>
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
